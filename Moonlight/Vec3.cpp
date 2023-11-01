4a8dc2a0a9839b7d0cf8f40a9fd5d80d15a3da7a
Segward504 <149601774+Segward504@users.noreply.github.com>
2023-11-01 22:44:32 +0100

    Update Vec3.cpp

--- Moonlight/Vec3.cpp
+++ Moonlight/Vec3.cpp
@@ -5,20 +5,17 @@
 
 #include "Vec3.hpp"
 
-Vec3::Vec3(double ax, double ay, double az) {
-    x = ax;
-    y = ay;
-    z = az;
+Vec3::Vec3(double x, double y, double z) {
+    a = {x, y, z};
 }
 
 double Vec3::Dot(Vec3 b) {
-    return x * b.x + y * b.y + z * b.z;
-    
+    return a[0] * b.a[0] + a[1] * b.a[1] + a[2] * b.a[2];
 }
 
 double Vec3::Normal() {
-    // getting the magnitude but squared
-    double Normal = pow(x + y + z, 2);
+    // getting the normal but squared
+    double Normal = pow(a[0] + a[1] + a[2], 2);
     // returning the root of the square
     return sqrt(Normal);
 }
@@ -29,15 +26,15 @@ Vec3 Vec3::Unit() {
     // getting multiplier needed to downscale
     double m = 1/n;
     // bring Vec3 down in scale
-    Vec3 u = Vec3(x * m, y * m, z * m);
+    Vec3 u = Vec3(a[0] * m, a[1] * m, a[2] * m);
     return u;
 }
 
-Vec3 Vec3::Subtract(Vec3 b) {
-    return Vec3(x - b.x, y - b.y, z - b.z);
+Vec3 Vec3::Sub(Vec3 b) {
+    return Vec3(a[0] - b.a[0], a[1] - b.a[1], a[2] - b.a[2]);
 }
 
-Vec3 Vec3::Addition(Vec3 b) {
-    return Vec3(x + b.x, y + b.y, z + b.z);
+Vec3 Vec3::Add(Vec3 b) {
+    return Vec3(a[0] + b.a[0], a[1] + b.a[1], a[2] + b.a[2]);
 }
 
