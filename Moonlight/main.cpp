9c37e5eb54f8959258757ba14c496a981f3dee05
Segward504 <149601774+Segward504@users.noreply.github.com>
2023-11-01 22:44:11 +0100

    Update main.cpp

--- Moonlight/main.cpp
+++ Moonlight/main.cpp
@@ -6,22 +6,25 @@
 #include <iostream>
 #include "Vec3.hpp"
 #include "Population.hpp"
+#include "DNN.hpp"
 
 int main(int argc, const char * argv[]) {
     
     int Generations = 100;
     int PopulationSize = 100;
     
-    Population Pop = Population(PopulationSize);
+    DNN a = DNN();
+    // assigned random weights
+    a.Generate();
     
-    // test of dot:
-    // Vec3 Position = Vec3(5, 0, 5);
-    // Vec3 LookVector = Vec3(-1,0,-1);
-    // Vec3 Target = Vec3(0, 0, 0);
-    // Vec3 Delta = Target.Subtract(Position).Unit();
-    // double Dot = Delta.Dot(LookVector);
-    // std::cout << Dot << "\n";
-
+    DNN b = DNN();
+    // assigned random weights
+    b.Generate();
+    
+    DNN c = DNN();
+    // assigned a split weights inherited from a and b
+    c.Inherit(a, b);
+    
     return 0;
 }
 
